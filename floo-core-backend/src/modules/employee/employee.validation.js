71469a59228ca86c6962617328465645b249f008
unknown <wadangalap@gmail.com>
2026-04-22 23:09:03 +0700

    validation empl

--- floo-core-backend/src/modules/employee/employee.validation.js
+++ floo-core-backend/src/modules/employee/employee.validation.js
@@ -4,22 +4,30 @@ const Joi = require("joi");
 const createEmployeeSchema = Joi.object({
   name: Joi.string().required(),
   position: Joi.string().required(),
-  phone: Joi.string().optional(),
-  address: Joi.string().optional(),
+  phone: Joi.string().allow("", null),
+  address: Joi.string().allow("", null),
 
   salary: Joi.number().required(),
   salary_type: Joi.string().valid("weekly", "monthly").required(),
+
+  // 🔥 TAMBAHAN WAJIB (SUPABASE FILE)
+  photo: Joi.string().allow("", null),
+  ktp_photo: Joi.string().allow("", null),
 });
 
 // UPDATE
 const updateEmployeeSchema = Joi.object({
   name: Joi.string().optional(),
   position: Joi.string().optional(),
-  phone: Joi.string().optional(),
-  address: Joi.string().optional(),
+  phone: Joi.string().allow("", null),
+  address: Joi.string().allow("", null),
 
   salary: Joi.number().optional(),
   salary_type: Joi.string().valid("weekly", "monthly").optional(),
+
+  // 🔥 TAMBAHAN WAJIB
+  photo: Joi.string().allow("", null),
+  ktp_photo: Joi.string().allow("", null),
 });
 
 // PARAM
